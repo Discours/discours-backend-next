954c3740cd65caf890db6e8b27def4526820dcb0
Untone <anton.rewin@gmail.com>
2023-12-12 08:00:46 +0300

    authorizer-connector-fix-7

--- services/auth.py
+++ services/auth.py
@@ -6,34 +6,35 @@ from settings import AUTH_URL
 
 async def check_auth(req):
     token = req.headers.get("Authorization")
-    print(f"[services.auth] checking auth token: {token}")
+    if token:
+        print(f"[services.auth] checking auth token: {token}")
 
-    headers = {"Authorization": "Bearer " + token, "Content-Type": "application/json"}
-    # query getSession($params: SessionQueryInput){ session(params: $params) { message user { id } } }
-    gql = {
-        "query": "query GetSession($params: SessionQueryInput)"
-        + "{ session(params: $params) { message user { id } } }",
-        "operationName": "GetSession",
-        "variables": {},
-    }
+        headers = {"Authorization": "Bearer " + token, "Content-Type": "application/json"}
+        # query getSession($params: SessionQueryInput){ session(params: $params) { message user { id } } }
+        gql = {
+            "query": "query GetSession($params: SessionQueryInput)"
+            + "{ session(params: $params) { message user { id } } }",
+            "operationName": "GetSession",
+            "variables": {},
+        }
 
-    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=30.0)) as session:
-        async with session.post(AUTH_URL, headers=headers, json=gql) as response:
-            if response.status != 200:
-                return False, None
-            r = await response.json()
-            print(f"[services.auth] response: {r}")
-            try:
-                data = r.get("data")
-                is_authenticated = False
-                user_id = None
-                if data:
-                    user_id = data.get("session", {}).get("user", {}).get("id", None)
-                    is_authenticated = user_id is not None
-                return is_authenticated, user_id
-            except Exception as e:
-                print(f"{e}: {r}")
-                return False, None
+        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=30.0)) as session:
+            async with session.post(AUTH_URL, headers=headers, json=gql) as response:
+                if response.status != 200:
+                    return False, None
+                r = await response.json()
+                print(f"[services.auth] response: {r}")
+                try:
+                    data = r.get("data")
+                    is_authenticated = False
+                    user_id = None
+                    if data:
+                        user_id = data.get("session", {}).get("user", {}).get("id", None)
+                        is_authenticated = user_id is not None
+                    return is_authenticated, user_id
+                except Exception as e:
+                    print(f"{e}: {r}")
+    return False, None
 
 
 def login_required(f):
