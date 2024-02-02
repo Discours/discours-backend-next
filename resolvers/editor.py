516945ddecbad48307f2a2e3dd20d84a2775c898
Untone <anton.rewin@gmail.com>
2024-02-02 21:04:21 +0300

    publish-fix

--- resolvers/editor.py
+++ resolvers/editor.py
@@ -153,6 +153,8 @@ async def update_shout(  # noqa: C901
 
                 # Replace datetime with Unix timestamp
                 shout_input['updated_at'] = current_time  # Set updated_at as Unix timestamp
+                if publish:
+                    shout_input['published_at'] = current_time
                 Shout.update(shout, shout_input)
                 session.add(shout)
 
