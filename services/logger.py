e85c179d93ae86291466f7f14fe80efd1864a593
Untone <anton.rewin@gmail.com>
2024-02-20 18:16:17 +0300

    muiltilinelog

--- services/logger.py
+++ services/logger.py
@@ -2,25 +2,29 @@ import logging
 import colorlog
 
 def get_colorful_logger(name):
-    # Создаем объект форматирования для цветовой разметки
+    # Define the color scheme
+    color_scheme = {
+        'DEBUG': 'light_black',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red,bg_white',
+    }
+
+    # Create a ColoredFormatter object for colorized logging
     formatter = colorlog.ColoredFormatter(
         "%(log_color)s%(levelname)-8s%(reset)s %(yellow)s[%(name)s]%(reset)s %(white)s%(message)s",
-        log_colors={
-            'DEBUG': 'light_black',
-            'INFO': 'green',
-            'WARNING': 'yellow',
-            'ERROR': 'red',
-            'CRITICAL': 'red,bg_white',
-        },
+        log_colors=color_scheme,
         secondary_log_colors={},
-        style='%'
+        style='%',
+        reset=True
     )
 
-    # Создаем поток вывода для записи журнала
+    # Create a stream handler for logging output
     stream = logging.StreamHandler()
     stream.setFormatter(formatter)
 
-    # Создаем логгер
+    # Create and configure the logger
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     logger.addHandler(stream)
