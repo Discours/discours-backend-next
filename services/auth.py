a962435898f123f54e93023a1c54121891f79552
Untone <anton.rewin@gmail.com>
2024-02-27 13:55:11 +0300

    root-auth-logs-3

--- services/auth.py
+++ services/auth.py
@@ -9,6 +9,8 @@ from settings import ADMIN_SECRET, AUTH_URL
 trace_logger = logging.getLogger('_trace')
 trace_logger.setLevel(logging.INFO)
 
+httpx_logger = logging.getLogger('httpx')
+trace_logger.setLevel(logging.INFO)
 
 async def request_data(gql, headers=None):
     if headers is None:
