605d60f126b005c4ef85e9f41514af031926cab2
Untone <anton.rewin@gmail.com>
2024-04-25 14:01:16 +0300

    featured-filter-fix

--- resolvers/reader.py
+++ resolvers/reader.py
@@ -59,9 +59,9 @@ def apply_filters(q, filters, author_id=None):
                 ),
             )
 
-        by_featured = filters.get("featured", "")
-        if isinstance(by_featured, bool) and by_featured:
-            q = q.filter(Shout.featured_at > Shout.published_at)
+        by_featured = filters.get("featured")
+        if by_featured:
+            q = q.filter(Shout.featured_at.is_not(None))
         by_layouts = filters.get("layouts")
         if by_layouts:
             q = q.filter(Shout.layout.in_(by_layouts))
