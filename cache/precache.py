c5a5e449d43cdb023e99461076fe18750cb7f9ca
Untone <anton.rewin@gmail.com>
2024-08-07 11:38:34 +0300

    precache-fix

--- cache/precache.py
+++ cache/precache.py
@@ -83,10 +83,10 @@ async def precache_data():
             # topics
             topics = get_with_stat(select(Topic))
             for topic in topics:
-                topic_profile = topic.dict() if not isinstance(topic, dict) else topic
-                await cache_topic(topic_profile)
+                topic_dict = topic.dict() if not isinstance(topic, dict) else topic
+                await cache_topic(topic_dict)
                 await asyncio.gather(
-                    precache_topics_followers(topic["id"], session), precache_topics_authors(topic["id"], session)
+                    precache_topics_followers(topic["id"], session), precache_topics_authors(topic_dict["id"], session)
                 )
             logger.info(f"{len(topics)} topics and their followings precached")
 
