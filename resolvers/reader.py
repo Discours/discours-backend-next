af2d8caebe4cdcf6c6789dce2eec2a58cd4dd849
Untone <anton.rewin@gmail.com>
2024-11-12 18:52:48 +0300

    toler-none2

--- resolvers/reader.py
+++ resolvers/reader.py
@@ -162,7 +162,8 @@ def query_with_stat(info):
             .group_by(Reaction.shout)
             .subquery()
         )
-        author_id = info.context.get("author", {}).get("id") if info.context else None
+        author_dict = info.context.get("author") if info.context else None
+        author_id = author_dict.get("id") if author_dict else None
         user_reaction_subquery = None
         if author_id:
             user_reaction_subquery = (
